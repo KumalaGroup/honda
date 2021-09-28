1175e5e83c135ccaefd8e354af4f319a97f43a51
ismailherianto <ianlotus35@gmail.com>
2021-09-28 16:57:22 +0800

    update curl

--- app/Helpers/curl_helper.php
+++ app/Helpers/curl_helper.php
@@ -3,6 +3,8 @@
     $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
     $r = curl_exec($curl);
     curl_close($curl);
     return $r;
@@ -13,6 +15,8 @@ function curl_get($url)
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
     $r = curl_exec($curl);
     curl_close($curl);
     return $r;
